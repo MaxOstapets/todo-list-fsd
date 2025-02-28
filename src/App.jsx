7b4d3ee943992b433f74a2b9ae5d973be8e53759
MaxOstapets <maksostapets2007@gmail.com>
2025-03-01 00:17:56 +0200

    made add todo function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import { LuSunMedium } from "react-icons/lu";
 import { FiPlusCircle } from "react-icons/fi";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([
+    {
+      todo: "Help",
+      complete: false,
+      id: Math.floor(Math.random() * 1000)
+    },
+    {
+      todo: "Go for a walk",
+      complete: false,
+      id: Math.floor(Math.random() * 1000)
+    },
+  ])
+
+  const handlerTodo = () => {
+    setTodos([...todos, {todo, complete: false, id: Math.floor(Math.random() * 1000)}])
+    setTodo("")
+  }
+
   return (
     <main className="w-full bg-indigo-700 h-full flex justify-center items-center">
       <section className="w-[500px] text-white mt-40">
@@ -10,18 +30,20 @@ function App() {
           <LuSunMedium className="text-xl"/>
         </div>
         <div className="flex justify-between items-center gap-10">
-          <input type="text" className="w-full bg-slate-800 p-[20px] mb-5 rounded-md" placeholder="Task"/>
-          <button><FiPlusCircle className="text-3xl"/></button>
+          <input type="text" className="w-full bg-slate-800 p-[20px] mb-5 rounded-md" placeholder="Task" value={todo} onChange={(el) => setTodo(el.target.value)}/>
+          <button onClick={handlerTodo}><FiPlusCircle className="text-3xl"/></button>
         </div>
         <div className="w-full bg-slate-800 text-white rounded-md">
             <ul>
-              <li className="p-[20px] flex gap-2">
+              {todos.map((el) => 
+              <li className="p-[20px] flex gap-2" key={el.id}>
                 <input type="checkbox" />
-                <span>hello</span>
+                <span>{el.todo}</span>
               </li>
+              )}
             </ul>
             <div className="text-xs flex justify-between items-center px-[20px] py-[10px] text-gray-400 bg-slate-800">
-              <p>5 items left</p>
+              <p>{todos.length} items left</p>
               <ul className="flex gap-2 text-sm text-blue-50">
                 <li>All</li>
                 <li>Active</li>
